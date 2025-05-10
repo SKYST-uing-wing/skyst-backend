b6dc3e2c6018bd40ec97545bef7e595f1312d4e7
HK Lee <69958270+tteokgook1g@users.noreply.github.com>
2025-05-10 22:13:22 +0900

    new: result, compare gateway

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
+
+from numpy import dot
+from numpy.linalg import norm
+import numpy as np
+
 
 import shutil
 import os
+
+from wav2vec import wav2vec
 
 app = FastAPI()
 
@@ -32,4 +40,40 @@ async def upload_mp3(name: str, file: UploadFile = File(...)):
     with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
+    convert_and_save_vec(name)
+
     return {"message": "File uploaded successfully", "filename": file.filename}
+
+
+def convert_and_save_vec(name):
+    file_path = Path(f'./uploads/{name}.wav')
+    vec = wav2vec(file_path)
+    np.save('./vectors/{name}.npy', vec)
+
+
+def get_vec(name):
+    file_path = Path(f'./vectors/{name}.npy')
+    if file_path.exists():
+        return np.load(file_path)
+    file_path = Path(f'./vectors/{name}.wav')
+    if not file_path.exists():
+        raise HTTPException(status_code=404, detail="File not found")
+    convert_and_save_vec(name)
+    return get_vec(name)
+
+
+@app.get('/result')
+async def result(name: str):
+    vec = get_vec(name)
+    print(vec)
+    return vec.tolist()
+
+
+@app.get('/compare')
+async def compare(name: str, target: str):
+    vec1 = get_vec(name).flatten()
+    vec2 = get_vec(target).flatten()
+    print(vec1[:5], vec2[:5])
+    cos_sim = dot(vec1, vec2)/(norm(vec1)*norm(vec2))
+    print(cos_sim)
+    return cos_sim.item()
