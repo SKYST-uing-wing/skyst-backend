363423d3c4cad9c305a88bbc9e196071f2ca864b
ysg142857 <ysg142857@gmail.com>
2025-05-10 22:11:25 +0900

    wav2vec implementation (5120 size -> (10, 6) size)

--- wav2vec.py
+++ wav2vec.py
@@ -1,6 +1,7 @@
 import numpy as np
 import openl3
 import soundfile as sf
+from sklearn.decomposition import PCA
 
 def audio_preprocessing(audio, defaultlen = 16000 * 5):
   audio_length = audio.shape[0]
@@ -18,6 +19,18 @@ def wav2vec(path):
     audio, sr = sf.read(path)
     audio_preprocessed = audio_preprocessing(audio, sr*5)
     emb, ts = openl3.get_audio_embedding(audio_preprocessed, sr=sr, embedding_size=512, hop_size=0.5)
-    vec = emb.flatten()
-    return vec
     
+    emb_tot = np.load("C:/Users/ysg14/Desktop/skyst/emb_tot_test_512_arr.npy")
+    
+    vec = []
+    for i in range(10):
+        pca = PCA(n_components=5)
+        pca.fit(emb_tot[:,i,:])
+        
+        amp = [audio_preprocessed[(sr//2)*i : (sr//2)*(i+1)].max()]
+        
+        pca_output = list(pca.transform(emb[i].reshape(1, -1))[0])
+        
+        vec.append(amp + pca_output)
+    
+    return np.array(vec)
